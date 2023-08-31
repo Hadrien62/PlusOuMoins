6af23811e7cd56be7ca172e831570fd72375edb5
Hadrien62 <hadrien.delobel@student.junia.com>
2023-08-31 09:58:07 +0200

    Initial Commit

---
+++ PlusOuMoins/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PlusOuMoins
+//
+//  Created by Hadrien Delobel on 31/08/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
