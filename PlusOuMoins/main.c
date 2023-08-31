fb6c976a518ddb08250b629dca00ffba04835460
Hadrien62 <hadrien.delobel@student.junia.com>
2023-08-31 20:16:12 +0200

    Jeu Plus ou moins

--- PlusOuMoins/main.c
+++ PlusOuMoins/main.c
@@ -6,9 +6,28 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 100
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    printf("Le but de ce code est de trouvé un nombre aléatoire avec pour seul indication + ou - \n");
+    srand(time(NULL));
+    int NbTrouve = rand() % MAX;
+    int proposition;
+    printf("Entrez un nombre pour voir si il est correct tant que vous avez pas trouve le bon \n");
+    do{
+        scanf("%d", &proposition);
+        if(proposition < NbTrouve){
+            printf("Le nombre %d est moins \n", proposition);
+        }
+        else if(proposition > NbTrouve){
+            printf("Le nombre %d est plus \n", proposition);
+        }
+        else if(proposition == NbTrouve){
+            printf("Bravo vous avez trouve !!! \nLe nombre etait bien %d \n", NbTrouve);
+        }
+    }while(proposition != NbTrouve);
     return 0;
 }
